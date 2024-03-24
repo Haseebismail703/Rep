9bbcbe0648aec3136582470568c4871ea38c618d
haseebismail703 <haseebismail703@gmail.com>
2024-03-24 17:08:16 +0500

    ecma 1

---
+++ ECMA/practice/app.js
@@ -0,0 +1,26 @@
+// var a = 'Haseeb'
+// var a = 'ismail'
+// a = 'haseeb'
+// console.log(a)
+
+// let a = 'Haseeb'
+// a = 'ismail'
+// console.log(a)
+
+// const a = 'Haseeb'
+// console.log(a)
+
+// if(1==1){
+//     var a = 'Haseeb'
+// }
+// console.log(a);
+
+// if(1 == 1){
+// let b = 'Haseeb'
+// }
+// console.log(b);
+
+// if(1 == 1){
+//     const b = 'Haseeb'
+//     }
+//     console.log(b);
